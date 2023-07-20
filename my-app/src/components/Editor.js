b136133218ce864eb2ac30b4d2e09817b7779492
Kishorenaath <Kishorenaath8@gmail.com>
2023-07-20 21:00:05 +0530

    sobha

--- my-app/src/components/Editor.js
+++ my-app/src/components/Editor.js
@@ -5,6 +5,8 @@ import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Editor(props) {
   const {
@@ -27,7 +29,7 @@ export default function Editor(props) {
           type="button"
           className="expand-collapse-btn"
           onClick={() => setOpen(prevOpen => !prevOpen)}
-        >o/c
+        > <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
 
         </button>
       </div>
